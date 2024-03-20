803300c2570d2291fb1d5d1964375b4d18011177
tmkerley <70177694+tmkerley@users.noreply.github.com>
2024-03-19 22:17:54 -0600

    heroku

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
 <?php 
-require('app/model/database.php');
+include 'views/public_header.php';
+require('model/database.php');
 require('model/classes_db.php');
 require('model/types_db.php');
 require('model/vehicle_db.php');
-
-include 'views/public_header.php';
 
 $sortType = filter_input(INPUT_POST, 'sortType');
 if(empty($sortType)) {
