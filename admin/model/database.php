2bf0f8c6ce871306ff0b0a572368beccc7e0a36a
tmkerley <70177694+tmkerley@users.noreply.github.com>
2024-03-19 23:58:01 -0600

    heroku bs

--- admin/model/database.php
+++ admin/model/database.php
@@ -1,10 +1,10 @@
 <?php
-    $dsn = 'mysql://tjfiklp35p367iqd:ekk7if6nfig7gkx2@gzp0u91edhmxszwf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/w2rm76kscxad3b8d;';
+    $dsn = 'mysql:host=gzp0u91edhmxszwf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;port=3306;dname=w2rm76kscxad3b8d;';
     $username = 'tjfiklp35p367iqd';
     $password = 'ekk7if6nfig7gkx2';
 
     try {
-        $db = new PDO($dsn, $username, $password, $port);
+        $db = new PDO($dsn, $username, $password);
     }catch(PDOExeption $e) {
         $error_message = $e->getMessage();
         include('../errors/database_error.php');
