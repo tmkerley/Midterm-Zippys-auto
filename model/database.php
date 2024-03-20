bfd970a7a6159945edaac886d058be09bb69236f
tmkerley <70177694+tmkerley@users.noreply.github.com>
2024-03-20 15:16:35 -0600

    heroku bs

--- model/database.php
+++ model/database.php
@@ -1,10 +1,11 @@
 <?php
-    $dsn = 'mysql:host=gzp0u91edhmxszwf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;port=3306;dname=w2rm76kscxad3b8d;';
+    $dsn = 'mysql:host=gzp0u91edhmxszwf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com; dname=w2rm76kscxad3b8d; port=3306;';
     $username = 'tjfiklp35p367iqd';
     $password = 'ekk7if6nfig7gkx2';
     
     try {
         $db = new PDO($dsn, $username, $password);
+        echo "Connection successful.";
     }catch(PDOExeption $e) {
         $error_message = $e->getMessage();
         include('../errors/db_error.php');
