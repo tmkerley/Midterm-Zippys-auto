87dc6e6d227392dfe3f668769ff943222cb7ca4e
tmkerley <70177694+tmkerley@users.noreply.github.com>
2024-03-19 22:11:29 -0600

    db name update

--- admin/model/database.php
+++ admin/model/database.php
@@ -3,10 +3,6 @@
     $username = 'tjfiklp35p367iqd';
     $password = 'ekk7if6nfig7gkx2';
 
-    // $dsn = 'mysql:host=localhost; dbname=ZippyAuto';
-    // $username = 'theadmin';
-    // $password = 'pa55word';
-    
     try {
         $db = new PDO($dsn, $username, $password);
     }catch(PDOExeption $e) {
