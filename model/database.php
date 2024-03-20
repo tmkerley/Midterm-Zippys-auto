b795e7d716d08536cc0f2a2a9b32c81a2e7c7e24
tmkerley <70177694+tmkerley@users.noreply.github.com>
2024-03-19 22:55:53 -0600

    heroku

--- model/database.php
+++ model/database.php
@@ -1,7 +1,7 @@
 <?php
-    $dsn = 'mysql:host=localhost; dbname=ZippyAuto';
-    $username = 'theadmin';
-    $password = 'pa55word';
+    $dsn = 'mysql://tjfiklp35p367iqd:ekk7if6nfig7gkx2@gzp0u91edhmxszwf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/w2rm76kscxad3b8d; dbname=w2rm76kscxad3b8d';
+    $username = 'tjfiklp35p367iqd';
+    $password = 'ekk7if6nfig7gkx2';
     
     try {
         $db = new PDO($dsn, $username, $password);
