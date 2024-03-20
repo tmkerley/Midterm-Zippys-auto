37da4ef306a67127a8b2835e6f3360b6ea5a0ef8
tmkerley <70177694+tmkerley@users.noreply.github.com>
2024-03-19 23:32:04 -0600

    heroku connection errors

--- admin/model/database.php
+++ admin/model/database.php
@@ -2,9 +2,10 @@
     $dsn = 'mysql://tjfiklp35p367iqd:ekk7if6nfig7gkx2@gzp0u91edhmxszwf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/w2rm76kscxad3b8d; dbname=w2rm76kscxad3b8d';
     $username = 'tjfiklp35p367iqd';
     $password = 'ekk7if6nfig7gkx2';
+    $port = '3306';
 
     try {
-        $db = new PDO($dsn, $username, $password);
+        $db = new PDO($dsn, $username, $password, $port);
     }catch(PDOExeption $e) {
         $error_message = $e->getMessage();
         include('../errors/database_error.php');
