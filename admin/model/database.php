961ade796d660dcddaffeb129da0a3bcfd3fe12a
tmkerley <70177694+tmkerley@users.noreply.github.com>
2024-03-19 22:09:26 -0600

    db name update

--- admin/model/database.php
+++ admin/model/database.php
@@ -1,5 +1,5 @@
 <?php
-    $dsn = 'gzp0u91edhmxszwf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+    $dsn = 'mysql:gzp0u91edhmxszwf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com; dbname=w2rm76kscxad3b8d';
     $username = 'tjfiklp35p367iqd';
     $password = 'ekk7if6nfig7gkx2';
 
