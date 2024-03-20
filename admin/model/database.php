e1c37c800c14223a3ad0689bcd5d003c30bcd73b
tmkerley <70177694+tmkerley@users.noreply.github.com>
2024-03-19 21:38:04 -0600

    website troubleshooting

--- admin/model/database.php
+++ admin/model/database.php
@@ -1,7 +1,11 @@
 <?php
-    $dsn = 'mysql:host=localhost; dbname=ZippyAuto';
-    $username = 'theadmin';
-    $password = 'pa55word';
+    $dsn = 'gzp0u91edhmxszwf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+    $username = 'tjfiklp35p367iqd';
+    $password = 'ekk7if6nfig7gkx2';
+
+    // $dsn = 'mysql:host=localhost; dbname=ZippyAuto';
+    // $username = 'theadmin';
+    // $password = 'pa55word';
     
     try {
         $db = new PDO($dsn, $username, $password);
