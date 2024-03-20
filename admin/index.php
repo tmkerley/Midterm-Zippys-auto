8304991cc6ec85f02824726668363f707b6a7ffa
tmkerley <70177694+tmkerley@users.noreply.github.com>
2024-03-20 00:22:29 -0600

    model file path update

--- admin/index.php
+++ admin/index.php
@@ -1,11 +1,11 @@
 <?php 
 include './views/admin_header.php';
 
-require './model/database.php';
-require('./model/classes_db.php');
-require('./model/types_db.php');
-require('./model/make_db.php');
-require('./model/vehicle_db.php');
+require '../model/database.php';
+require('../model/classes_db.php');
+require('../model/types_db.php');
+require('../model/make_db.php');
+require('../model/vehicle_db.php');
 
 $action = filter_input(INPUT_POST, 'action');
 if ($action == NULL) {
