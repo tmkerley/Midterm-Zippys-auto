84d85091e4ec2dfafcf49e4d2b9531ab9a6f939d
tmkerley <70177694+tmkerley@users.noreply.github.com>
2024-03-20 18:12:28 -0600

    heroku bs

--- model/database.php
+++ model/database.php
@@ -1,5 +1,5 @@
 <?php
-    $dsn = 'mysql:host=gzp0u91edhmxszwf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=w2rm76kscxad3b8d;port=3306;';
+    $dsn = 'mysql:host=gzp0u91edhmxszwf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=w2rm76kscxad3b8d;';
     $username = 'tjfiklp35p367iqd';
     $password = 'ekk7if6nfig7gkx2';
     
