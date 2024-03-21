f31d173d2cd084e10b6c02a9273cff485ab7a365
tmkerley <70177694+tmkerley@users.noreply.github.com>
2024-03-20 18:29:23 -0600

    database connection

--- model/database.php
+++ model/database.php
@@ -5,8 +5,6 @@
     
     try {
         $db = new PDO($dsn, $username, $password);
-        // set the PDO error mode to exception
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         echo "Connected successfully";
     }catch(PDOExeption $e) {
         $error_message = $e->getMessage();
